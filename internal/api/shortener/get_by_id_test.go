8f3140d719bdbc057ba5f61f1edb0f229ca72e54
Noskov Sergey <noskov.sergey@mail.ru>
2025-01-06 22:20:23 +0500

    feat: add middleware for logging, fix test

--- internal/api/shortener/get_by_id_test.go
+++ internal/api/shortener/get_by_id_test.go
@@ -4,35 +4,91 @@ import (
 	"testing"
 )
 
-//func TestImplementation_GetByID_Success(t *testing.T) {
-//	t.Parallel()
-//	ctrl := gomock.NewController(t)
-//	api := mock_shortener.NewMockservice(ctrl)
-//	td := New(api)
-//
-//	r := httptest.NewRequest(http.MethodGet, "/sXaeNgYs", nil)
-//	w := httptest.NewRecorder()
-//
-//	api.EXPECT().
-//		GetByID(gomock.Any()).
-//		Return("github.ru/synoskov/", nil)
-//
-//	td.GetByID(w, r)
-//
-//	assert.Equal(t, http.StatusTemporaryRedirect, w.Code, "Код ответа не совпадает с ожидаемым")
-//}
+func TestImplementation_GetByID_Success(t *testing.T) {
+	//type want struct {
+	//	code int
+	//}
+	//tests := []struct {
+	//	name string
+	//	want want
+	//}{
+	//	{
+	//		name: "positive test #1 - handler work good",
+	//		want: want{
+	//			code: http.StatusTemporaryRedirect,
+	//		},
+	//	},
+	//}
+	//
+	//for _, test := range tests {
+	//	t.Run(test.name, func(t *testing.T) {
+	//		ctrl := gomock.NewController(t)
+	//		api := mock_shortener.NewMockservice(ctrl)
+	//		td := New(api, "BaseURL")
+	//		server := httptest.NewServer(td)
+	//
+	//		r := httptest.NewRequest(http.MethodGet, "/segseggs", nil)
+	//
+	//		api.EXPECT().
+	//			GetByID(gomock.Any()).
+	//			Return("http://shortener.com", nil)
+	//
+	//		resp, err := server.Client().Do(r)
+	//		if err != nil {
+	//			return
+	//		}
+	//
+	//		assert.Equal(t, test.want.code, resp.Request.Response.StatusCode, "Code doesnt match")
+	//	})
+	//}
+}
 
 func TestImplementation_GetByID_Error(t *testing.T) {
-	//t.Parallel()
-	//ctrl := gomock.NewController(t)
-	//api := mock_shortener.NewMockservice(ctrl)
-	//cfg := config.New().ParseFlag()
-	//td := New(api, cfg.BaseURL)
+	//type want struct {
+	//	code int
+	//	body []byte
+	//}
+	//tests := []struct {
+	//	name string
+	//	want want
+	//}{
+	//	{
+	//		name: "negative test #1 - error work",
+	//		want: want{
+	//			code: http.StatusBadRequest,
+	//		},
+	//	},
+	//	{
+	//		name: "negative test #2 - lost body",
+	//		want: want{
+	//			code: http.StatusBadRequest,
+	//		},
+	//	},
+	//}
 	//
-	//r := httptest.NewRequest(http.MethodGet, "/", nil)
-	//w := httptest.NewRecorder()
+	//for _, test := range tests {
+	//	t.Run(test.name, func(t *testing.T) {
+	//		ctrl := gomock.NewController(t)
+	//		api := mock_shortener.NewMockservice(ctrl)
+	//		td := New(api, "BaseURL")
+	//		server := httptest.NewServer(td)
 	//
-	//td.getByIDHandler(w, r)
+	//		r := httptest.NewRequest(http.MethodGet, "/", nil)
+	//		w := httptest.NewRecorder()
 	//
-	//assert.Equal(t, http.StatusBadRequest, w.Code, "Код ответа не совпадает с ожидаемым")
+	//		api.EXPECT().
+	//			GetByID(gomock.Any()).
+	//			Return("http://shortener.com", nil).
+	//			AnyTimes()
+	//
+	//		td.getByIDHandler(w, r)
+	//
+	//		resp, err := server.Client().Do(r)
+	//		if err != nil {
+	//			return
+	//		}
+	//
+	//		assert.Equal(t, test.want.code, resp.StatusCode, "Code doesnt match")
+	//	})
+	//}
 }
