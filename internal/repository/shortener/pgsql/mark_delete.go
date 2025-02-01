9fa17f4041c73e589f68b74f605f6697c32fa356
Noskov Sergey <noskov.sergey@mail.ru>
2025-02-01 14:21:26 +0500

    feat: add delete

--- internal/repository/shortener/pgsql/mark_delete.go
+++ internal/repository/shortener/pgsql/mark_delete.go
@@ -10,11 +10,11 @@ func (r *Repository) MarkDelete(data []string) error {
 		return fmt.Errorf("begin: %w", err)
 	}
 
-	for _, shortUrl := range data {
+	for _, shortURL := range data {
 		_, err = tx.Exec(`
 		UPDATE shortener SET is_deleted = TRUE
 		WHERE short_url = $1`,
-			shortUrl)
+			shortURL)
 		if err != nil {
 			tx.Rollback()
 			return fmt.Errorf("failed exec tx: %w", err)
